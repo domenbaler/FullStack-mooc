20c048abf1fd5cc19035f1f3f601e39dda3aefc2
domen <domen.baler@gmail.com>
2021-09-14 09:54:20 +0200

    added data fetching to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,30 +1,28 @@
-import React, {useState} from 'react'
-import Filter from './components/Filter'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setNewFilter] = useState('')
   
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => {setNewName(event.target.value)}
+  const handleNumberChange = (event) => {setNewNumber(event.target.value)}
+  const handleFilterChange = (event) => {setNewFilter(event.target.value)}
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  const handleFilterChange = (event) => {
-    setNewFilter(event.target.value)
-  }
+  useEffect( () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  },[])
+ 
 
   const addPerson = (event) => {
     if (persons.some(person => person.name===newName)){
@@ -40,7 +38,6 @@ const App = () => {
       setNewName('')
       setNewNumber('')
     }
-
   }
 
   return (
